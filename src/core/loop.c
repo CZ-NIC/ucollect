fa400552e0e01cba906b1dbcc08d960bdea7c77c
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-02-13 14:43:00 +0100

    [core] Register PCAP fd in epoll
    When the PCAP is created, register it with the epoll instance, so we can
    know when it is ready.

--- src/core/loop.c
+++ src/core/loop.c
@@ -3,11 +3,23 @@
 #include "util.h"
 
 #include <signal.h> // for sig_atomic_t
-#include <pcap/pcap.h>
 #include <assert.h>
 #include <string.h> // Why is memcpy in string?
+#include <errno.h>
+#include <unistd.h>
+
+#include <pcap/pcap.h>
+#include <sys/epoll.h>
 
 struct pcap_interface {
+	/*
+	 * This will be always set to pcap_read. Trick to make the epollhandling
+	 * simpler ‒ we put this struct directly as the user data to epoll and the
+	 * generic handler calls the handler found, no matter what type it is.
+	 */
+	void (*handler)(struct pcap_interface *);
+	// Link back to the loop owning this pcap. For epoll handler.
+	struct loop *loop;
 	const char *name;
 	pcap_t *pcap;
 	int fd;
@@ -18,15 +30,39 @@ struct loop {
 	struct mem_pool *permanent_pool;
 	struct pcap_interface *pcap_interfaces;
 	size_t pcap_interface_count;
+	int epoll_fd;
 	sig_atomic_t stopped; // We may be stopped from a signal, so not bool
 };
 
+static void pcap_read(struct pcap_interface *interface) {
+	ulog(LOG_DEBUG, "Read on interface %s\n", interface->name);
+}
+
+static void epoll_register_pcap(struct loop *loop, size_t index, int op) {
+	struct epoll_event event = {
+		.events = EPOLLIN,
+		.data = {
+			.ptr = loop->pcap_interfaces + index
+		}
+	};
+	if (epoll_ctl(loop->epoll_fd, op, loop->pcap_interfaces[index].fd, &event) == -1)
+		die("Can't register PCAP fd %d of %s to epoll fd %d (%s)\n", loop->pcap_interfaces[index].fd, loop->pcap_interfaces[index].name, loop->epoll_fd, strerror(errno));
+}
+
 struct loop *loop_create() {
 	ulog(LOG_INFO, "Creating a main loop\n");
+	/*
+	 * 42 is arbitrary choice. The man page says it is ignored except it must
+	 * be positive number.
+	 */
+	int epoll_fd = epoll_create(42);
+	if (epoll_fd == -1)
+		die("Couldn't create epoll instance (%s)\n", strerror(errno));
 	struct mem_pool *pool = mem_pool_create("Global permanent pool");
 	struct loop *result = mem_pool_alloc(pool, sizeof *result);
 	*result = (struct loop) {
-		.permanent_pool = pool
+		.permanent_pool = pool,
+		.epoll_fd = epoll_fd
 	};
 	return result;
 }
@@ -44,6 +80,9 @@ void loop_run(struct loop *loop) {
 
 void loop_destroy(struct loop *loop) {
 	ulog(LOG_INFO, "Releasing the main loop\n");
+	// Close the epoll
+	int result = close(loop->epoll_fd);
+	assert(result == 0);
 	// Close all PCAPs
 	for (size_t i = 0; i < loop->pcap_interface_count; i ++) {
 		ulog(LOG_INFO, "Closing PCAP on %s\n", loop->pcap_interfaces[i].name);
@@ -62,6 +101,8 @@ static const size_t ip_offset_table[] =
 	[DLT_RAW] = 0,     /* RAW IP             */
 	[DLT_PFLOG] = 28,  /* BSD pflog          */
 };
+
+
 
 bool loop_add_pcap(struct loop *loop, const char *interface) {
 	ulog(LOG_INFO, "Initializing PCAP on %s\n", interface);
@@ -129,15 +170,20 @@ bool loop_add_pcap(struct loop *loop, const char *interface) {
 	 * But it is not really clean solution.
 	 */
 	struct pcap_interface *interfaces = mem_pool_alloc(loop->permanent_pool, (loop->pcap_interface_count + 1) * sizeof *interfaces);
-	// Copy the old interfaces
+	// Copy the old interfaces (and re-register with epoll, to change the pointer)
 	memcpy(interfaces, loop->pcap_interfaces, loop->pcap_interface_count * sizeof *interfaces);
+	for (size_t i; i < loop->pcap_interface_count; i ++)
+		epoll_register_pcap(loop, i, EPOLL_CTL_MOD);
 	assert(pcap_datalink(pcap) <= DLT_PFLOG);
 	interfaces[loop->pcap_interface_count ++] = (struct pcap_interface) {
+		.handler = pcap_read,
+		.loop = loop,
 		.name = mem_pool_strdup(loop->permanent_pool, interface),
 		.pcap = pcap,
 		.fd = fd,
 		.offset = ip_offset_table[pcap_datalink(pcap)]
 	};
 	loop->pcap_interfaces = interfaces;
+	epoll_register_pcap(loop, loop->pcap_interface_count - 1, EPOLL_CTL_ADD);
 	return true;
 }
