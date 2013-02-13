d96427ee7989b4902b0c9f19a31cca6f3a983062
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-02-13 11:27:05 +0100

    [core] Minimal logging support
    Provide just the log level and message. The log level is currently
    ignored and everything is just printed to stderr.

--- src/core/util.c
+++ src/core/util.c
@@ -11,3 +11,11 @@ void die(const char *format, ...) {
 	va_end(args);
 	exit(1);
 }
+
+void ulog(enum log_level log_level, const char *format, ...) {
+	(void) log_level; // Currently ignored
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+}
--- src/core/util.h
+++ src/core/util.h
@@ -3,4 +3,13 @@
 
 void die(const char *format, ...) __attribute__((format(printf, 1, 2))) __attribute__((noreturn));
 
+enum log_level {
+	LOG_ERROR,
+	LOG_WARN,
+	LOG_INFO,
+	LOG_DEBUG
+};
+
+void ulog(enum log_level log_level, const char *format, ...) __attribute__((format(printf, 2, 3)));
+
 #endif
