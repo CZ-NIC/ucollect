2ab34f56ebfba1d91d462e2fa52856464d18262f
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-02-14 15:08:55 +0100

    [core] Provide the context struct
    That should hold information about plugins and for plugins.

---
+++ src/core/context.h
@@ -0,0 +1,18 @@
+#ifndef UCOLLECT_CONTEXT_H
+#define UCOLLECT_CONTEXT_H
+
+struct mem_pool;
+
+struct context {
+	struct mem_pool *permanent_storage;
+	struct mem_pool *temp_storage;
+	void *user_data;
+};
+
+/*
+ * Currently active context. For things like signal handlers and debug,
+ * not to be used directly. A copy should be always passed to the callback.
+ */
+extern struct context *current_context;
+
+#endif
