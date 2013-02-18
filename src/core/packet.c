dbf52ab1b8bc77d62417de6f189551d93dc4c055
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-02-18 13:48:07 +0100

    [core] Parse UDP/TCP ports & header length

--- src/core/packet.c
+++ src/core/packet.c
@@ -7,6 +7,29 @@
 #include <stdbool.h>
 #include <string.h>
 
+// UDP header is 4 2-byte words long
+#define UDP_LENGTH 8
+// All the sizes of headers are specified in 4-byte chunks
+#define HEADER_SIZE_UNIT 4
+// The offset resides in the high 4 bytes of the offset byte
+#define OFFSET_MASK 0xf0
+// The offset is in the high half of the offset byte, shift by 4 bits to get the actual number
+#define OFFSET_SHIFT 4
+
+/*
+ * Both TCP and UDP have the same start, the ports. In case of TCP, we use the
+ * data offset too (which should still be masked out), in UDP it may fall out
+ * of the packet. But we won't access it, so it doesn't matter. Therefore we abuse
+ * it to UDP too.
+ */
+struct tcp_ports {
+	uint16_t sport;
+	uint16_t dport;
+	uint32_t seq_num;
+	uint32_t ack_num;
+	uint8_t offset;
+};
+
 static void parse_internal(struct packet_info *packet) {
 	/*
 	 * Try to put the packet into the form for an IP packet. We're lucky, the version field
@@ -26,7 +49,7 @@ static void parse_internal(struct packet_info *packet) {
 			packet->addresses[END_SRC] = &iphdr->saddr;
 			packet->addresses[END_DST] = &iphdr->daddr;
 			// Temporary length, for further parsing (IP only).
-			packet->hdr_length = 4 * iphdr->ihl;
+			packet->hdr_length = HEADER_SIZE_UNIT * iphdr->ihl;
 			protocol = iphdr->protocol;
 			break;
 		case 6: {
@@ -52,25 +75,49 @@ static void parse_internal(struct packet_info *packet) {
 	}
 	// TODO: Guess the direction by the addresses.
 	packet->direction = DIR_UNKNOWN;
-	// Should we extract ports from the next header?
-	bool extract_ports = false;
-	// How large is the next header (if known)?
-	// TODO:
-	size_t add_length = 0;
+	/*
+	 * The start of the next header. Might be tcp, or something else (we abuse the structure
+	 * for UDP too).
+	 *
+	 * As C doesn't allow adding an integer to a void pointer, we cast to pointer to unsigned char.
+	 * But then we need to cast to the target pointer, since it isn't auto-convertible without
+	 * warning.
+	 */
+	const struct tcp_ports *tcp_ports = (const struct tcp_ports *) (((const uint8_t *) packet->data) + packet->hdr_length);
+	size_t length_rest = packet->length - packet->hdr_length;
+	// Default protocol is unknown.
+	packet->app_protocol = '?';
 	switch (protocol) {
 		// Just hardcode the numbers here for our use.
 		case 6: // TCP
+			if (length_rest < sizeof *tcp_ports)
+				/*
+				 * It claims to be TCP, but it's too short for that.
+				 * Actually, even if it passes this check, it may be
+				 * too short for TCP, but it's long enough for the
+				 * info we want, so we won't crash. Likely, such broken
+				 * packets won't even get to us, but who knows for
+				 * sure.
+				 */
+				return;
 			packet->app_protocol = 'T';
-			extract_ports = true;
+			// FIXME: This doesn't seem to work :-(.
+			packet->hdr_length += HEADER_SIZE_UNIT * ((tcp_ports->offset & OFFSET_MASK) >> OFFSET_SHIFT);
 			break;
 		case 17: // UDP
 			packet->app_protocol = 'U';
-			extract_ports = true;
+			if (length_rest < UDP_LENGTH)
+				// Too short for UDP
+				return;
+			packet->hdr_length += UDP_LENGTH;
 			break;
 		default:
-			packet->app_protocol = '?';
-			break;
+			// Something unknown. Keep the '?'
+			return;
 	}
+	// Extract the ports
+	packet->ports[END_SRC] = ntohs(tcp_ports->sport);
+	packet->ports[END_DST] = ntohs(tcp_ports->dport);
 }
 
 // Zero or reset the some fields according to other fields, if they don't make sense in that context.
