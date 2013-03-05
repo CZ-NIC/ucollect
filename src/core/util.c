7b2c6a3d8de18160c92eedd39091a3eb38f9f78e
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-03-05 13:19:16 +0100

    [core] Provide core on die()
    Use abort(), not exit(1), so a core file is created if it is allowed.

--- src/core/util.c
+++ src/core/util.c
@@ -8,7 +8,7 @@ void die(const char *format, ...) {
 	va_start(args, format);
 	vfprintf(stderr, format, args);
 	va_end(args);
-	exit(1);
+	abort();
 }
 
 void ulog_internal(enum log_level log_level, const char *format, va_list *args) {
