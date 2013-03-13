59232de1db25946ca090301dbd3e1058f6f3692a
Michal 'vorner' Vaner <vorner@vorner.cz>
2013-03-13 11:31:02 +0100

    [buckets] More criteria to hash by
    It is possible to hash by port and also by the address+port pair.

--- src/plugins/buckets/criteria.c
+++ src/plugins/buckets/criteria.c
@@ -6,9 +6,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
+#include <arpa/inet.h>
 
 // IPv6 is 16 bytes long, preceded by the version byte. We pad v4 by zeroes.
 #define ADDR_SIZE 17
+#define PORT_SIZE sizeof(uint16_t)
 
 static bool copy_ip(uint8_t *where, const struct packet_info *packet) {
 	enum endpoint remote = remote_endpoint(packet->direction);
@@ -31,11 +33,48 @@ static const uint8_t *extract_ip_address(const struct packet_info *packet, struc
 	return result;
 }
 
+static bool copy_port(uint8_t *where, const struct packet_info *packet) {
+	enum endpoint remote = remote_endpoint(packet->direction);
+	if (remote == END_COUNT)
+		return false;
+	if (packet->ports[remote] == 0)
+		return false;
+	uint16_t port_net = htons(packet->ports[remote]);
+	memcpy(where, &port_net, sizeof port_net);
+	return true;
+}
+
+static const uint8_t *extract_port(const struct packet_info *packet, struct mem_pool *tmp_pool) {
+	uint8_t *result = mem_pool_alloc(tmp_pool, PORT_SIZE);
+	if (copy_port(result, packet))
+		return result;
+	else
+		return NULL;
+}
+
+static const uint8_t *extract_both(const struct packet_info *packet, struct mem_pool *tmp_pool) {
+	uint8_t *result = mem_pool_alloc(tmp_pool, PORT_SIZE + ADDR_SIZE);
+	if (copy_port(result, packet) && copy_ip(result + PORT_SIZE, packet))
+		return result;
+	else
+		return NULL;
+}
+
 struct criterion_def criteria[] = {
 	{ // Remote address
 		.key_size = ADDR_SIZE,
 		.name = 'I',
 		.extract_key = extract_ip_address
+	},
+	{ // Remote port
+		.key_size = PORT_SIZE,
+		.name = 'P',
+		.extract_key = extract_port
+	},
+	{ // Both port and address
+		.key_size = PORT_SIZE + ADDR_SIZE,
+		.name = 'B',
+		.extract_key = extract_both
 	},
 	{ // Sentinel
 		.name = '\0'
