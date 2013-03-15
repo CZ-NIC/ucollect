e931378321d3681d6af1fbca3eba1d5fb2ef5102
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-03-15 14:16:14 +0100

    [count] Cleanup
    • For each thing, capture both count and size
    • Use better array with symbolic constants for arrays
    • Send both the statistics and the counts/sizes in one message
    • Include timestamp to know the length of interval

--- src/plugins/count/count.c
+++ src/plugins/count/count.c
@@ -7,15 +7,33 @@
 #include "../../core/loop.h"
 
 #include <arpa/inet.h>
+#include <assert.h>
+#include <string.h>
+#include <endian.h>
 
-struct user_data {
-	size_t count;
-	size_t count_v6, count_v4;
-	size_t count_in, count_out;
-	size_t count_tcp, count_udp, count_icmp;
-	size_t low_port_count;
-	size_t size_in, size_out, size;
+enum selector {
+	ANY,
+	V4, V6,
+	IN, OUT,
+	TCP, UDP, ICMP,
+	LOW_PORT,
+	MAX
 };
+
+// This should get _no_ paddings, on 32bit nor 64bit systems
+struct user_data {
+	uint64_t timestamp;
+	struct {
+		uint32_t count;
+		uint32_t size;
+	} data[MAX];
+};
+
+static void update(struct user_data *d, const struct packet_info *info, enum selector selector) {
+	assert(selector < MAX);
+	d->data[selector].count ++;
+	d->data[selector].size += info->length - info->hdr_length;
+}
 
 static void packet_handle(struct context *context, const struct packet_info *info) {
 	struct user_data *d = context->user_data;
@@ -24,100 +42,87 @@ static void packet_handle(struct context *context, const struct packet_info *inf
 		packet_handle(context, info->next);
 		return;
 	}
-	d->count ++;
-	d->size += info->length - info->hdr_length;
+	update(d, info, ANY);
 	switch (info->direction) {
 		case DIR_IN:
-			d->count_in ++;
-			d->size_in += info->length - info->hdr_length;
+			update(d, info, IN);
 			break;
 		case DIR_OUT:
-			d->count_out ++;
-			d->size_out += info->length - info->hdr_length;
+			update(d, info, OUT);
 			break;
 		default:;// Ignore others (and silence warning)
 	}
 	switch (info->ip_protocol) {
 		case 4:
-			d->count_v4 ++;
+			update(d, info, V4);
 			break;
 		case 6:
-			d->count_v6 ++;
+			update(d, info, V6);
 			break;
 	}
 	switch (info->app_protocol) {
 		case 'T':
-			d->count_tcp ++;
+			update(d, info, TCP);
 			break;
 		case 'U':
-			d->count_udp ++;
+			update(d, info, UDP);
 			break;
 		case 'i':
 		case 'I':
-			d->count_icmp ++;
+			update(d, info, ICMP);
 	}
 	enum endpoint remote = remote_endpoint(info->direction);
 	if (remote != END_COUNT && info->ports[remote] <= 1024 && info->ports[remote] != 0)
-		d->low_port_count ++;
-	ulog(LOG_DEBUG_VERBOSE,
-"Statistics after received packet:\n"
-"Total app data size:	%7zu\n"
-"Size in:		%7zu\n"
-"Size out:		%7zu\n"
-"Total count:		%7zu\n"
-"IPv6:			%7zu\n"
-"IPv4:			%7zu\n"
-"IN:			%7zu\n"
-"OUT:			%7zu\n"
-"TCP:			%7zu\n"
-"UDP:			%7zu\n"
-"ICMP:			%7zu\n"
-"On low port:		%7zu\n"
-		, d->size, d->size_in, d->size_out, d->count, d->count_v6, d->count_v4, d->count_in, d->count_out, d->count_tcp, d->count_udp, d->count_icmp, d->low_port_count);
+		update(d, info, LOW_PORT);
 }
 
 static void initialize(struct context *context) {
 	context->user_data = mem_pool_alloc(context->permanent_pool, sizeof *context->user_data);
 	// We would initialize with {} to zero everything, but iso C doesn't seem to allow that.
 	*context->user_data = (struct user_data) {
-		.count = 0
+		.timestamp = 0
 	};
 }
 
+struct encoded {
+	uint64_t timestamp;
+	uint32_t if_count;
+	uint32_t data[];
+};
+
 static void communicate(struct context *context, const uint8_t *data, size_t length) {
-	// TODO: Error handling
-	if (length != 1)
-		return;
-	switch (*data) {
-		case 'D': {// Send and reset the data
-			struct user_data *d = context->user_data;
-			/*
-			 * Since the structure is just bunch of items of the same type,
-			 * we can look at it as an array. This allows us to convert
-			 * it in an easier way. It is a slight abuse, but it should
-			 * be legal according to the C standard (though it is probably
-			 * fragile) and this plugin is more like a proof of concept
-			 * anyway.
-			 */
-			size_t *items = (size_t *) d;
-			size_t item_count = sizeof *d / sizeof *items;
-			uint32_t message[item_count];
-			for (size_t i = 0; i < item_count; i ++) {
-				message[i] = htonl(items[i]);
-				items[i] = 0;
-			}
-			uplink_plugin_send_message(context, message, sizeof message);
-			break;
-		}
-		case 'S': {
-			size_t *stats = loop_pcap_stats(context);
-			uint32_t message[1 + 3 * *stats];
-			for (size_t i = 0; i < 1 + 3 * *stats; i ++)
-				message[i] = htonl(stats[i]);
-			uplink_plugin_send_message(context, message, sizeof message);
-			break;
-		}
+	if (length != sizeof(uint64_t))
+		die("Invalid request from upstream to plugin count, size %zu\n", length);
+	struct user_data *u = context->user_data;
+	// Extract timestamp for the next interval
+	uint64_t timestamp;
+	memcpy(&timestamp, data, length);
+	timestamp = be64toh(timestamp);
+	// Generate capture statistics
+	size_t *stats = loop_pcap_stats(context);
+	// Get enough space to encode the result
+	size_t enc_len = 3 * *stats + 2 * MAX;
+	struct encoded *encoded;
+	size_t enc_size = sizeof *encoded + enc_len * sizeof *encoded->data;
+	encoded = mem_pool_alloc(context->temp_pool, enc_size);
+	encoded->timestamp = htobe64(u->timestamp);
+	// Encode the statistics
+	encoded->if_count = htonl(*stats);
+	for (size_t i = 0; i < 3 * *stats; i ++)
+		encoded->data[i] = htonl(stats[i + 1]);
+	// Encode the counts & sizes
+	size_t offset = 3 * *stats;
+	for (size_t i = 0; i < MAX; i ++) {
+		encoded->data[2 * i + offset] = htonl(u->data[i].count);
+		encoded->data[2 * i + offset + 1] = htonl(u->data[i].size);
 	}
+	// Send the message
+	uplink_plugin_send_message(context, encoded, enc_size);
+	// Reset the statistics
+	*u = (struct user_data) {
+		.timestamp = timestamp
+		// The rest are zeroes
+	};
 }
 
 struct plugin *plugin_info(void) {
