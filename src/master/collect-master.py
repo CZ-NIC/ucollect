0282b50360935a47f6b9cca647877ea146b9ea3a
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-04-03 15:48:28 +0200

    Initialize logging

--- src/master/collect-master.py
+++ src/master/collect-master.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP6ServerEndpoint
+import logging
 from client import ClientFactory
 from plugin import Plugins
 import count_plugin
 import buckets_plugin
 
+logging.basicConfig(level=logging.DEBUG, format='%(name)s@%(module)s:%(lineno)s\t%(asctime)s\t%(levelname)s\t%(message)s')
 plugins = Plugins()
 count_plugin.CountPlugin(plugins)
 buckets_plugin.BucketsPlugin(plugins)
 # Some configuration, to load the port from?
 endpoint = TCP6ServerEndpoint(reactor, 5678)
 endpoint.listen(ClientFactory(plugins))
+logging.info('Init done')
 reactor.run()
