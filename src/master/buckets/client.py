60e90c030cb6ad928fbfb1d0a1da713e8cf24d04
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-04-09 16:12:44 +0200

    [master] Document the client class

--- src/master/buckets/client.py
+++ src/master/buckets/client.py
@@ -5,11 +5,26 @@ import struct
 logger = logging.getLogger(name='buckets')
 
 class RequestManager:
+	"""
+	Manager keeping track of requests sent to the clients and their IDs.
+	"""
 	def __init__(self):
 		self.__requests = {}
 		self.__id = 0
 
 	def register(self, callback, timeout):
+		"""
+		Register another request. Pass a callback to be called with the received
+		answer and a timeout (absolute number of seconds since the epoch), after
+		which the callback will be discarded without being called if the client
+		doesn't answer.
+
+		The callback is called as `callback(data, success)`, the data is the answer
+		from client and success is True in case the client provides the answer.
+		If the client sends error, it is called as `callback(None, False)`.
+
+		This method returns new ID to be used for the request.
+		"""
 		nid = self.__id
 		self.__id += 1
 		self.__id %= 2^32
@@ -17,6 +32,9 @@ class RequestManager:
 		return nid
 
 	def trim(self):
+		"""
+		Drop the callbacks that have timed out.
+		"""
 		t = time.time()
 		to_del = []
 		for k in self.__requests:
@@ -36,12 +54,23 @@ class RequestManager:
 			logging.warn("Response for unknown request %s received, ignoring", rid)
 
 	def response(self, rid, data):
+		"""
+		A response from client with the given request ID and data came. The
+		method will call the corresponding callback.
+		"""
 		self.__route(rid, data, True)
 
 	def missing(self, rid):
+		"""
+		The data to satisfy request with the given request ID is missing on the client.
+		The method will call the corresponding callback with success=False.
+		"""
 		self.__route(rid, None, False)
 
 manager = RequestManager()
+"""
+Singleton instance of the RequestManager.
+"""
 
 class Client:
 	"""
@@ -49,22 +78,47 @@ class Client:
 	for keys, callbacks to them, etc.
 	"""
 	def __init__(self, name, groups, send):
+		"""
+		Create the client. Store the name and list of groups the client belongs to.
+
+		The send is a callable which may be used to send message to the remote client.
+		"""
 		self.__name = name
 		self.__groups = groups
 		self.__send = send
 
 	def groups(self):
+		"""
+		Return the list of groups passed to initialization.
+		"""
 		return self.__groups
 
 	def name(self):
+		"""
+		Return the name passed to the initialization.
+		"""
 		return self.__name
 
 	def get_keys(self, criterion, generation, keys, callback):
+		"""
+		Request keys for given buckets on the client.
+		- criterion: index of the criterion in configuration to ask keys for.
+		- generation: the timestamp when the generation started.
+		- keys: list of keys in the encoded list (eg. length, so many numbers,
+		  length, so many numbers, ...)
+		- callback: Called with the (raw) response from client when it comes. See
+		  the RequestManager to the format of the callback.
+		"""
 		# TODO: Caching of the results
 		rid = manager.register(callback, time.time() + 60) # One minute as a timeout is enough
 		message = struct.pack('!QLL' + str(len(keys)) + 'L', generation, rid, criterion, *keys)
 		self.__send('K' + message)
 
 	def deactivate(self):
+		"""
+		Notify the client already disconnected. Do not call any callbacks even if
+		the answer comes (seems impossible, but with event-based application, who
+		knows).
+		"""
 		pass
 		# TODO: Cancel callbacks/make sure they are not propagated anywhere. Can the callbacks happen now at all?
