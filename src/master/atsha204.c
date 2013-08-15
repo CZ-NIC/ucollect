c74100e3ba0c8fb7b96abf15f17f3d3fd8fa76a1
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-08-15 15:57:48 +0200

    The wrappers for atsha204

--- src/master/atsha204.c
+++ src/master/atsha204.c
@@ -5,17 +5,30 @@
 /*
  * As linking to other libraries from python extensions is tricky,
  * we do fully dynamic loading of the library with dlopen.
+ *
+ * Also, most of the error checking is done by assert instead of
+ * python exceptions, out of laziness.
  */
 
 static void *library;
 
 static PyObject *atsha_do_hmac(PyObject *self, PyObject *args) {
-	
+	(void) self;
+	int size_serial, size_key, size_challenge;
+	const uint8_t *serial, *key, *challenge;
+	if (!PyArg_ParseTuple(args, "s#s#s#", &serial, &size_serial, &key, &size_key, &challenge, &size_challenge))
+		return NULL;
+	assert(size_key == 32);
+	assert(size_challenge == 32);
+	struct atsha_handle *crypto = atsha_open_server_emulation(serial, key);
+	atsha_big_int challenge_s, response_s;
+	challenge_s.bytes = 32;
+	memcpy(challenge_s.data, challenge, 32);
+	int result = atsha_challenge_response(crypto, challenge_s, &response_s);
+	assert(result == ATSHA_ERR_OK);
+	atsha_close(crypto);
+	return Py_BuildValue("s#", response_s.data, (int) response_s.bytes);
 }
-
-struct atsha_handle *(*atsha_open_server_emulation_local)(const uint8_t *serial, const uint8_t *key);
-void (*atsha_close_local)(struct atsha_handle *handle);
-int (*atsha_challenge_response_local)(struct atsha_handle *handle, atsha_big_int challenge, atsha_big_int *response);
 
 static PyMethodDef atsha_methods[] = {
 	{"hmac", atsha_do_hmac, METH_VARARGS, NULL},
@@ -29,14 +42,5 @@ static void *get_sym(const char *name) {
 }
 
 PyMODINIT_FUNC initatsha204(void) {
-	library = dlopen("libatsha204.so", RTLD_NOW | RTLD_LOCAL | RTLD_DEEPBIND);
-	assert(library);
-	// Assignment from void * to function pointer is illegal, but needed by the dynamic loading.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-pedantic"
-	atsha_open_server_emulation_local = get_sym("atsha_open_server_emulation");
-	atsha_close_local = get_sym("atsha_close");
-	atsha_challenge_response_local = get_sym("atsha_challenge_response");
-#pragma GCC diagnostic pop
 	Py_InitModule("atsha204", atsha_methods);
 }
