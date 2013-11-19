df1ee69bc3e74c4ef2a7b23830a927d3efc76537
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-11-19 16:34:21 +0100

    Disallow some weaker ciphers
    Not that they'd be considered insecure, just extra paranoia.

--- src/core/uplink.c
+++ src/core/uplink.c
@@ -225,7 +225,7 @@ static bool uplink_connect_internal(struct uplink *uplink) {
 		}
 		close(sockets[1]);
 		close(errs[1]);
-		const char *remote = mem_pool_printf(loop_temp_pool(uplink->loop), "OPENSSL:%s:%s,cafile=%s,cipher=HIGH:!MEDIUM:!LOW:!aNULL,compress=auto,method=TLS", uplink->remote_name, uplink->service, uplink->cert);
+		const char *remote = mem_pool_printf(loop_temp_pool(uplink->loop), "OPENSSL:%s:%s,cafile=%s,cipher=HIGH:!LOW:!MEDIUM:!SSLv2:!aNULL:!eNULL:!DES:!3DES:!AES128:!CAMELLIA128,compress=auto,method=TLS", uplink->remote_name, uplink->service, uplink->cert);
 		execlp("socat", "socat", "STDIO", remote, (char *) NULL);
 		die("Exec should never exit but it did: %s\n", strerror(errno));
 	}
