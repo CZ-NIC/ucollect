d066fce81391ffcf459d7705e2776cb061e9cee1
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-11-29 09:44:11 +0100

    Unify copyright headers

--- src/plugins/buckets/hash.c
+++ src/plugins/buckets/hash.c
@@ -1,6 +1,6 @@
 /*
     Ucollect - small utility for real-time analysis of network data
-    Copyright (C) 2013 CZ.NIC
+    Copyright (C) 2013 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
