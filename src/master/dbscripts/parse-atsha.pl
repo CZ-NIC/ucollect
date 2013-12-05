f9c92d62885ec0a2d5b93b958df66ca0c37b25ae
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-12-05 15:46:16 +0100

    We import turrises now, not blackboxes

--- src/master/dbscripts/parse-atsha.pl
+++ src/master/dbscripts/parse-atsha.pl
@@ -6,7 +6,7 @@ $/ = "\n\n";
 my @input = <STDIN>;
 
 my $slot = 0;
-my $tag = 'blackbox';
+my $tag = 'turris-test';
 my $start = 3;
 
 print "BEGIN;\n";
@@ -21,10 +21,10 @@ for my $input (@input) {
 	$id = lc($id);
 	my $data = $data[$slot + 1];
 	print "INSERT INTO clients (name, passwd, mechanism, slot_id, tag, devel_note) VALUES('$id', '$keys[$slot]', 'A', $slot, '$tag', '$name');\n";
-	print "INSERT INTO groups (name) VALUES('bb$start');\n";
+	print "INSERT INTO groups (name) VALUES('T$start');\n";
 	print "INSERT INTO group_members (client, in_group) SELECT clients.id, groups.id FROM clients CROSS JOIN groups WHERE clients.name = '$id' AND groups.name = 'all';\n";
-	print "INSERT INTO group_members (client, in_group) SELECT clients.id, groups.id FROM clients CROSS JOIN groups WHERE clients.name = '$id' AND groups.name = 'blackbox';\n";
-	print "INSERT INTO group_members (client, in_group) SELECT clients.id, groups.id FROM clients CROSS JOIN groups WHERE clients.name = '$id' AND groups.name = 'bb$start';\n";
+	print "INSERT INTO group_members (client, in_group) SELECT clients.id, groups.id FROM clients CROSS JOIN groups WHERE clients.name = '$id' AND groups.name = 'turris';\n";
+	print "INSERT INTO group_members (client, in_group) SELECT clients.id, groups.id FROM clients CROSS JOIN groups WHERE clients.name = '$id' AND groups.name = 'T$start';\n";
 	$start ++;
 }
 print "COMMIT;\n";
