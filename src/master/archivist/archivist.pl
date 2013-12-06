ba7732701bbc27e00ea7775938f0f14152d0db44
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-12-06 14:41:08 +0100

    Archive the config tables
    Synchronize the tables with configuration data (groups, anomaly_types,
    count_types) from db.turris.cz to archive.turris.cz.
    
    Closes: #2954.

--- src/master/archivist/archivist.pl
+++ src/master/archivist/archivist.pl
@@ -14,3 +14,41 @@ sub connect_db($) {
 
 my $source = connect_db 'source';
 my $destination = connect_db 'destination';
+
+# Synchronize the configuration tables
+# We do this by reading all data from both databases and comparing. Ugly, but it works.
+my %config_tables = (
+	groups => [qw(id name)],
+	anomaly_types => [qw(code description)],
+	count_types => [qw(ord name description)],
+);
+
+
+while (my($table, $columns) = each %config_tables) {
+	print "Syncing config table $table\n";
+	# Dump the whole table from both databases
+	my $columns_commas = join ', ', @$columns;
+	my $select = "SELECT $columns_commas FROM $table";
+	my $config_get = sub {
+		my ($db) = @_;
+		my $array = $db->selectall_arrayref($select);
+		my %hash = map { $_->[0] => $_ } @$array;
+		($array, \%hash);
+	};
+	my ($src_arr, $src_hash) = $config_get->($source);
+	my ($dst_arr, $dst_hash) = $config_get->($destination);
+	# Compare the data from the tables
+	my $questions = join ', ', ('?')x@$columns;
+	while (my ($id, $row) = each %$src_hash) {
+		if (exists $dst_hash->{$id}) {
+			# The row is in both tables. Just check that they are the same.
+			my $src_row = join '"', @$row;
+			my $dst_row = join '"', @{$dst_hash->{$id}};
+			die "Mismatched config on table $table with ID $id\n" if $src_row ne $dst_row;
+		} else {
+			# The row is in source, but not destination. Push it there too.
+			$destination->do("INSERT INTO $table ($columns_commas) VALUES ($questions)", undef, @$row);
+		}
+		# We don't mind rows in destination but not in source. After all, destination is archive.
+	}
+}
