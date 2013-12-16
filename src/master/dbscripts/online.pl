297547369fd2c518a3e6b5cc3cba1944daf2d98b
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-12-16 10:57:32 +0100

    Don't show the data throughputs
    Because of restricted privileges.

--- src/master/dbscripts/online.pl
+++ src/master/dbscripts/online.pl
@@ -21,9 +21,7 @@ SELECT
 	aggregate.devel_note,
 	aggregate.tag,
 	aggregate.last,
-	activity_types.name,
-	sums.all,
-	sums.server
+	activity_types.name
 FROM (
 	SELECT
 		clients.id,
@@ -42,22 +40,6 @@ LEFT OUTER JOIN activities ON
 	AND activities.timestamp = aggregate.last
 LEFT OUTER JOIN activity_types ON
 	activities.activity = activity_types.id
-LEFT OUTER JOIN (
-	SELECT
-		SUM(CASE WHEN (count_types.name = 'All') THEN counts.count ELSE 0 END) as all,
-		SUM(CASE WHEN (count_types.name = 'SERVER') THEN counts.size ELSE 0 END) as server,
-		count_snapshots.client
-	FROM
-		counts
-	JOIN count_snapshots ON
-		 counts.snapshot = count_snapshots.id
-	JOIN count_types ON
-		 counts.type = count_types.id
-	WHERE
-		timestamp > NOW() - interval '1 day'
-	GROUP BY count_snapshots.client
-) AS sums ON
-	sums.client = aggregate.id
 ORDER BY
 	aggregate.tag ASC,
 	aggregate.devel_note ASC;
@@ -65,18 +47,13 @@ ENDSQL
 while (1) {
 	my ($online, $offline) = (0, 0);
 	$stmt->execute;
-	print "${blue}ID\t\t\tStatus\tAct.\tTime\t\t\t\tCount\t\tServer\t\tNote$reset\n";
+	print "${blue}ID\t\t\tStatus\tAct.\tTime\t\t\t\tNote$reset\n";
 	my $now = time;
 	my $stuck = $now - 3600;
-	while (my ($name, $note, $tag, $last, $activity, $pcount, $ssize) = $stmt->fetchrow_array) {
+	while (my ($name, $note, $tag, $last, $activity) = $stmt->fetchrow_array) {
 		my $status = (defined $activity && $activity ne 'logout') ? "${green}online" : "${red}offline";
 		$last //= '----';
 		$activity //= '----';
-		$pcount //= '----';
-		$ssize //= '----';
-		my $pcolor = color 'reset';
-		$pcolor = color 'magenta' if $pcount < 1000000;
-		$pcolor = color 'red' if $pcount < 1000;
 		my $time = str2time($last // '0');
 		if ($time <= $stuck) {
 			$status = "${red}stuck" unless $status eq "${red}offline";
@@ -87,7 +64,7 @@ while (1) {
 		} else {
 			$offline ++;
 		}
-		printf "%-16s\t%s$reset\t%s\t%-30s\t$pcolor%10s$reset\t%10s\t%s\n", $name, $status, $activity, $last, $pcount, $ssize, $note;
+		printf "%-16s\t%s$reset\t%s\t%-30s\t%10s$reset\t%10s\t%s\n", $name, $status, $activity, $last, $note;
 	}
 	print "${green}Online:\t\t\t$online\n${red}Offline:\t\t$offline$reset\n";
 
