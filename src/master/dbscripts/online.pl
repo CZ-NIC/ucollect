47a535a44f17cd00e69298db38611623e494df12
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-12-16 11:22:58 +0100

    Skip completely inactive devices by default

--- src/master/dbscripts/online.pl
+++ src/master/dbscripts/online.pl
@@ -2,6 +2,7 @@
 use common::sense;
 use Term::ANSIColor;
 use Date::Parse;
+use Getopt::Long;
 use DBI;
 
 my $dbh = DBI->connect("dbi:Pg:dbname=routers", "ucollect", "", { RaiseError => 1, AutoCommit => 0 });
@@ -10,6 +11,14 @@ my $green = color 'bold green';
 my $red = color 'bold red';
 my $blue = color 'bold blue';
 my $reset = color 'reset';
+
+my $forever;
+my $all;
+
+GetOptions
+	forever => \$forever,
+	all => \$all
+	or die "Bad parameters\n";
 
 # TODO: This query is probably slower than we would like (especially the second nested
 # SELECT, because it goes through a lot of counts data). No idea what this'll do on
@@ -52,7 +61,6 @@ while (1) {
 	my $stuck = $now - 3600;
 	while (my ($name, $note, $tag, $last, $activity) = $stmt->fetchrow_array) {
 		my $status = (defined $activity && $activity ne 'logout') ? "${green}online" : "${red}offline";
-		$last //= '----';
 		$activity //= '----';
 		my $time = str2time($last // '0');
 		if ($time <= $stuck) {
@@ -64,12 +72,14 @@ while (1) {
 		} else {
 			$offline ++;
 		}
+		next unless $all or defined $last;
+		$last //= '----';
 		printf "%-16s\t%s$reset\t%s\t%-30s\t%10s$reset\t%10s\t%s\n", $name, $status, $activity, $last, $note;
 	}
 	print "${green}Online:\t\t\t$online\n${red}Offline:\t\t$offline$reset\n";
 
 	$dbh->rollback;
-	last if ($ARGV[0] ne '-f');
+	last if not $forever;
 	sleep 15;
 	print "\033[2J";
 }
