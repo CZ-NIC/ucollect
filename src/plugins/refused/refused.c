15856caf582eb4d784ee0a77930e7c7398037acc
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2014-10-02 12:16:32 +0200

    Skeleton of the refused plugin
    Start a plugin to track outgoing TCP connections that are refused. Still
    just empty file, to start building on.
    
    References: #3647.

---
+++ src/plugins/refused/refused.c
@@ -0,0 +1,32 @@
+/*
+    Ucollect - small utility for real-time analysis of network data
+    Copyright (C) 2014 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "../../core/plugin.h"
+
+#ifdef STATIC
+struct plugin *plugin_info_refused(void) {
+#else
+struct plugin *plugin_info(void) {
+#endif
+	static struct plugin plugin = {
+		.name = "Refused",
+		.version = 1
+	};
+	return &plugin;
+}
