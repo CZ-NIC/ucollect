f5547cdc5eadcdfe5a62400bb870a82d51a3c3b9
Robin Obůrka <robin.oburka@nic.cz>
2015-01-09 08:17:10 +0100

    [bandwidth] Use at least 2 windows frames
    Using of only 1 frame magically generates a lot of seconds.
    
    Timestamps are in microseconds and "Drop windows" event doesn't use epsilon
    sensitive comparison. This causes a lot of drop events followed by rewind
    events. Every rewind is adding 2 seconds to sum.

--- src/plugins/bandwidth/bandwidth.c
+++ src/plugins/bandwidth/bandwidth.c
@@ -408,12 +408,13 @@ void init(struct context *context) {
 
 	// Windows settings
 	// Parameter count should be number that windows_count*window_length is at least 1 second
+	// WARNING: Minimal value of windows_count is 2!
 	size_t init = 0;
 	context->user_data->windows[init++] = init_window(context->permanent_pool, 500000, 4, common_start_timestamp);
-	context->user_data->windows[init++] = init_window(context->permanent_pool, 1000000, 1, common_start_timestamp);
-	context->user_data->windows[init++] = init_window(context->permanent_pool, 2000000, 1, common_start_timestamp);
-	context->user_data->windows[init++] = init_window(context->permanent_pool, 5000000, 1, common_start_timestamp);
-	context->user_data->windows[init++] = init_window(context->permanent_pool, 10000000, 1, common_start_timestamp);
+	context->user_data->windows[init++] = init_window(context->permanent_pool, 1000000, 2, common_start_timestamp);
+	context->user_data->windows[init++] = init_window(context->permanent_pool, 2000000, 2, common_start_timestamp);
+	context->user_data->windows[init++] = init_window(context->permanent_pool, 5000000, 2, common_start_timestamp);
+	context->user_data->windows[init++] = init_window(context->permanent_pool, 10000000, 2, common_start_timestamp);
 
 	init = 0;
 	for (size_t i = 0; i <= 20; i++) {
