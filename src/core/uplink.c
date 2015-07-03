15f95902f5f32cbfa268c13310b6454c7c3066aa
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2015-07-03 12:39:48 +0200

    Try TLS 1.2

--- src/core/uplink.c
+++ src/core/uplink.c
@@ -284,7 +284,7 @@ static bool uplink_connect_internal(struct uplink *uplink) {
 		 * We could do something more clever, but this is simple and works.
 		 * See ticket #3106.
 		 */
-		const char *remote = mem_pool_printf(loop_temp_pool(uplink->loop), "OPENSSL:%s:%s,cafile=%s,cipher=HIGH:!LOW:!MEDIUM:!SSLv2:!aNULL:!eNULL:!DES:!3DES:!AES128:!CAMELLIA128,method=TLS1.0,pf=ip%d", uplink->remote_name, uplink->service, uplink->cert, uplink->last_ipv6 ? 6 : 4);
+		const char *remote = mem_pool_printf(loop_temp_pool(uplink->loop), "OPENSSL:%s:%s,cafile=%s,cipher=HIGH:!LOW:!MEDIUM:!SSLv2:!aNULL:!eNULL:!DES:!3DES:!AES128:!CAMELLIA128,method=TLS1.2,pf=ip%d", uplink->remote_name, uplink->service, uplink->cert, uplink->last_ipv6 ? 6 : 4);
 		ulog(LLOG_DEBUG, "Starting socat with %s\n", remote);
 		execlp("socat", "socat", "STDIO", remote, (char *) NULL);
 		die("Exec should never have exited but it did: %s\n", strerror(errno));
