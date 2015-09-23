0d1fc731aa107a97f6a6cc59f7924765eb9c32a2
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2015-09-23 15:28:52 +0200

    Some more documentation about pluglibs

--- src/core/pluglib_macros.h
+++ src/core/pluglib_macros.h
@@ -17,16 +17,48 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+/*
+ * This header helps manipulating with the custom "linker" we have for pluglibs.
+ * It allows for defining the needed structures and functions.
+ *
+ * In the pluglib, you create a header file with PLUGLIB_FUNC() macros. Inclusion
+ * of this file precedes the macros.
+ *
+ * Define PLUGLIB_DO_EXPORT in the pluglib's implementation and then include the
+ * header file. It'll create the function headers and bunch of function_name_export
+ * variables which can be linked from the pluglib description.
+ *
+ * Similarly, define PLUGLIB_DO_IMPORT in the plugin that uses the functions to one
+ * of the values in pluglib.h and include the header.
+ *
+ * You can include the header without any of these PLUGLIB_DO_* macros, but you get
+ * only the other stuff that is not linked by the "linker".
+ */
+
+// Cleanup, drop macros created by previous inclusion of the header.
 #ifdef PLUGLIB_IMPORT
 #undef PLUGLIB_IMPORT
 #undef PLUGLIB_EXPORT
 #undef PLUGLIB_FUNC
 #endif
 
+/*
+ * WARNING:
+ * Macro magic ahead. While it is explained, know what you're doing when you edit it.
+ *
+ * The basic principle here is, the includer of the header file specifies what
+ * should and should not be imported/exported and how. According to that, macros
+ * PLUGLIB_IMPORT and PLUGLIB_EXPORT are created. They are either doing the
+ * import/export or they are empty (so they can be called, but generate no code).
+ *
+ * The PLUGLIB_FUNC then calls both these macros.
+ */
+
+// The import macro. It distinguishes by the value of PLUGLIB_DO_IMPORT
 #ifdef PLUGLIB_DO_IMPORT
 #if PLUGLIB_DO_IMPORT == PLUGLIB_FUNCTIONS
 #define PLUGLIB_IMPORT(NAME, RETURN, ...) extern RETURN (*NAME)(__VA_ARGS__);
-#elif PLUGLIB_DO_IMPORT == PLUGLIB_STRUCTS
+#elif PLUGLIB_DO_IMPORT == PLUGLIB_PUBLIC
 #define PLUGLIB_IMPORT(NAME, RETURN, ...) RETURN (*NAME)(__VA_ARGS__); static struct pluglib_import NAME##_import = { .name = #NAME, .function = (pluglib_function *)&NAME, .prototype = #__VA_ARGS__ "->" #RETURN };
 #else
 #define PLUGLIB_IMPORT(NAME, RETURN, ...) static RETURN (*NAME)(__VA_ARGS__); static struct pluglib_import NAME##_import = { .name = #NAME, .function = (pluglib_function *)&NAME, .prototype = #__VA_ARGS__ "->" #RETURN };
@@ -36,10 +68,12 @@
 #define PLUGLIB_IMPORT(...)
 #endif
 
+// The export stuff. Currently only on/off.
 #ifdef PLUGLIB_DO_EXPORT
 #define PLUGLIB_EXPORT(NAME, RETURN, ...) static RETURN NAME(__VA_ARGS__); static struct pluglib_export NAME##_export = { .name = #NAME, .function = (pluglib_function)&NAME, .prototype = #__VA_ARGS__ "->" #RETURN };
 #else
 #define PLUGLIB_EXPORT(...)
 #endif
 
+// Perform both possible import and possible export.
 #define PLUGLIB_FUNC(...) PLUGLIB_IMPORT(__VA_ARGS__) PLUGLIB_EXPORT(__VA_ARGS__)
