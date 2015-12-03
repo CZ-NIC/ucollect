bb6a2c603fc6969adfffafe39faa19d40069ac99
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2015-12-03 13:24:14 +0100

    fwup: Warn about growing sizes of sets
    When the set size needs to be updated, warn about it, as the firewall
    definitions need to be updated.

--- src/master/dbscripts/build_fwup_sets.pl
+++ src/master/dbscripts/build_fwup_sets.pl
@@ -62,3 +62,8 @@ fake_set_generate 'turris_100FA4E0_lb_a_6_X', 6, 'hard';
 $dbh->do("UPDATE config SET value = ? WHERE plugin = 'fwup' AND name = 'version'", undef, $version) if $bump_config_version;
 
 $dbh->commit;
+
+# TODO: Solve the problem of propagating the size to the firewall rules somehow
+# Currently, if the sizes don't match, ucollect can't create the set. While we can switch
+# sets of different sizes, ipset -exist create complains.
+die "I had to update the sizes, please propagate them to the firewall rules.\n" if $bump_config_version;
