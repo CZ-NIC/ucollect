b5d0fab0b50dcef4a3d92b200e3b1fd3d83e07f1
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2016-12-16 15:21:37 +0100

    Experiment: Get the IP prefix to sort them out

--- src/master/amihacked/process/src/main.rs
+++ src/master/amihacked/process/src/main.rs
@@ -1,12 +1,16 @@
 extern crate csv;
+extern crate regex;
 
 use std::process::*;
 use std::thread;
+use std::sync::*;
+use regex::Regex;
 
 fn main() {
     let mut running = Vec::new();
     for arg in std::env::args() {
         let a_cp = arg.clone();
+        let prefix = Regex::new(r"^(.[^.]?)").unwrap();
         running.push(thread::spawn(move || {
             let mut unzip = Command::new("pbzip2").arg("-dc").arg(a_cp).stdout(Stdio::piped()).spawn().expect("Failed to start unzip");
             {
@@ -14,7 +18,9 @@ fn main() {
                 let mut reader = csv::Reader::from_reader(&mut output);
 
                 for row in reader.records() {
-                    println!("{:?}", row);
+                    let row = row.unwrap();
+                    let iprefix = prefix.captures(&row[0]).expect("Doesn't match").at(1).unwrap();
+                    println!("{} {}", &row[0], iprefix);
                 }
             }
 
