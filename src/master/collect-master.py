6bbfdfd21b1dfa14e333372dba70f667f8df8e28
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2017-01-30 08:56:56 +0100

    Revert "Try increasing the number of db connections"
    This reverts commit 58697377c6e425c018a00b22926d41bd84e8e2cc.

--- src/master/collect-master.py
+++ src/master/collect-master.py
@@ -32,7 +32,7 @@ import activity
 import importlib
 import os
 
-reactor.suggestThreadPoolSize(18) # Too much seems to have trouble with locking :-(
+reactor.suggestThreadPoolSize(4) # Too much seems to have trouble with locking :-(
 severity = master_config.get('log_severity')
 if severity == 'TRACE':
 	severity = log_extra.TRACE_LEVEL
